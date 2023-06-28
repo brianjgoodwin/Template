51d8c1d4d505a190c68bdc13ca09f27dff339bbe
Brian Goodwin <brianjgoodwin@gmail.com>
2023-06-27 19:49:14 -0700

    Initial Commit

---
+++ Template/TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemplateApp.swift
+//  Template
+//
+//  Created by Brian on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
